4e9896d37ae1d0ee3d704897bd3652ac96ade480
Souzx4 <eletelnext@gmail.com>
2026-07-22 22:01:23 -0300

    update

---
+++ src/maratonajava/array3.java
@@ -0,0 +1,19 @@
+package maratonajava;
+
+public class array3 {
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+        int[] numeros2 = {1, 2, 3, 4, 5};
+        int[] numeros3 = new int[]{1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numeros3.length; i++) {
+            System.out.println(numeros3[i]);
+        }
+
+        System.out.println("----------------");
+
+        for(int num:numeros2){
+            System.out.println(num);
+        }
+    }
+}
--- src/maratonajava/arrays2.java
+++ src/maratonajava/arrays2.java
@@ -7,8 +7,12 @@ public class arrays2 {
         // boolean false
         // String null
         String[] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        nomes[0] = "Goku";
+        nomes[1] = "Luffy";
+        nomes[2] = "Naruto";
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
     }
 }
